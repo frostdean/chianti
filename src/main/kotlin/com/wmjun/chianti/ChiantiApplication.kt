bac473458b98065488e95738654eeb9b7cb4be42
woominjun <minjun.woo@myrealtrip.com>
2020-07-09 21:28:28 +0900

    CH-00 init with spring-boot

---
+++ src/main/kotlin/com/wmjun/chianti/ChiantiApplication.kt
@@ -0,0 +1,11 @@
+package com.wmjun.chianti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChiantiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChiantiApplication>(*args)
+}
---
+++ src/test/kotlin/com/wmjun/chianti/ChiantiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wmjun.chianti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChiantiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
